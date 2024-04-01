377b284ff84887346889c1ba7dac0806a22ecca8
filipe.dias <fedispato@gmail.com>
2024-04-01 13:17:18 -0300

    feat: Adicionando estrutura de entidade e repository

---
+++ src/main/kotlin/tortora/avengers/avengersapi/resource/avenger/AvengerEntity.kt
@@ -0,0 +1,23 @@
+package tortora.avengers.avengersapi.resource.avenger
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import tortora.avengers.avengersapi.domain.avenger.Avenger
+
+@Entity
+data class AvengerEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+    @Column(nullable = false)
+    val nick: String,
+    @Column(nullable = false)
+    val person: String,
+    val description: String?,
+    val history: String?
+) {
+    fun toAvenger() = Avenger(id, nick, person, description, history)
+}
