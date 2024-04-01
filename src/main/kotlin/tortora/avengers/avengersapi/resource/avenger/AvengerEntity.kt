e5236e2a1852d047431358fe52ed59afe4dfefe0
filipe.dias <fedispato@gmail.com>
2024-04-01 17:06:43 -0300

    fix: avengerEntity com anotacao table

--- src/main/kotlin/tortora/avengers/avengersapi/resource/avenger/AvengerEntity.kt
+++ src/main/kotlin/tortora/avengers/avengersapi/resource/avenger/AvengerEntity.kt
@@ -5,9 +5,11 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 import tortora.avengers.avengersapi.domain.avenger.Avenger
 
 @Entity
+@Table(name = "avenger")
 data class AvengerEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
